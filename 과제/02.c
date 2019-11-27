62f544deabf1c1630cf866654835d8796b8382d7
Ryujongheon <lptprjh@office.bufs.ac.kr>
2019-11-27 14:41:20 +0900

    과제 02 수정

--- 과제/02.c
+++ 과제/02.c
@@ -8,13 +8,14 @@ int main(int argc, char const *argv[])
 
     // [여기에 핵심 요소를 입력]
     for(a=1;a<=9&&Q==-1;a++){
-        for(b=a;b<=9&&Q==-1;b++)
+        for(b=a;b<=9&&Q==-1;b++){
             for(c=b;c<=9&&Q==-1;c++){
                 tmp = a*b*c;
-                if(tmp==N){
+                if(tmp==N){ 
                     Q = a*100 + b*10 + c;
                 }
             }
+        }
     }
 
     if(Q/100==1) Q-=100;
